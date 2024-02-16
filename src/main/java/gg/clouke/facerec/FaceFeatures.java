2092237807ae8a8223f684e75aec7834d30830ae
Clouke <76826179+Clouke@users.noreply.github.com>
2024-02-16 12:50:51 +0100

    create `FaceFeatures`

---
+++ src/main/java/gg/clouke/facerec/FaceFeatures.java
@@ -0,0 +1,41 @@
+package gg.clouke.facerec;
+
+/**
+ * @author Clouke
+ * @since 16.02.2024 12:40
+ * © face-recognition - All Rights Reserved
+ */
+public enum FaceFeatures {
+  EYES,
+  NOSE,
+  MOUTH,
+  EYEBROWS,
+  CHEEKS,
+  JAWLINE,
+  CHIN,
+  EARS,
+  HAIR,
+  FACIAL_HAIR,
+  FOREHEAD,
+  TEETH,
+  SMILE,
+  DIMPLES,
+  FRECKLES,
+  WRINKLES,
+  MOLES,
+  SCARS,
+  BLUSH,
+  MAKEUP,
+  GLASSES,
+  HATS,
+  HEADWEAR,
+  EARRINGS,
+  NECKLACE,
+  PIERCINGS,
+  TATTOOS,
+  SKIN_TONE,
+  EXPRESSIONS,
+  AGE,
+  GENDER,
+  ETHNICITY
+}
