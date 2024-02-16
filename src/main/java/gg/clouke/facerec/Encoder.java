5913d78a9b2c71de61cd5405e215a1bafcba0298
Clouke <76826179+Clouke@users.noreply.github.com>
2024-02-16 12:50:33 +0100

    minor tweaks

--- src/main/java/gg/clouke/facerec/Encoder.java
+++ src/main/java/gg/clouke/facerec/Encoder.java
@@ -2,13 +2,11 @@ package gg.clouke.facerec;
 
 import gg.clouke.ContextVector;
 
-import java.awt.image.BufferedImage;
-
 /**
  * @author Clouke
  * @since 28.01.2024 13:26
  * © face-recognition - All Rights Reserved
  */
-public interface Encoder {
-  ContextVector encode(BufferedImage image);
+public interface Encoder<W> {
+  ContextVector encode(W image);
 }
