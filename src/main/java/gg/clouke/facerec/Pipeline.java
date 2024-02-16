10b9bd745691e1748e626c06c4146f9b91fa518e
Clouke <76826179+Clouke@users.noreply.github.com>
2024-02-16 12:50:41 +0100

    minor tweaks

--- src/main/java/gg/clouke/facerec/Pipeline.java
+++ src/main/java/gg/clouke/facerec/Pipeline.java
@@ -9,6 +9,12 @@ import java.awt.image.BufferedImage;
  * @since 16.02.2024 12:10
  * © face-recognition - All Rights Reserved
  */
-public interface Pipeline {
-  ContextVector process(BufferedImage image);
+public interface Pipeline<W> {
+
+  static PipelineBuilder<BufferedImage> builder() {
+    return new PipelineBuilder<>();
+  }
+
+  ContextVector process(W w);
+
 }
