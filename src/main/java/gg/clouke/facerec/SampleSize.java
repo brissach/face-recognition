9a0a05b6cfb52125eedf638e15f8b089748fd1b6
Clouke <76826179+Clouke@users.noreply.github.com>
2024-02-16 12:51:05 +0100

    create `SampleSize`

---
+++ src/main/java/gg/clouke/facerec/SampleSize.java
@@ -0,0 +1,38 @@
+package gg.clouke.facerec;
+
+/**
+ * @author Clouke
+ * @since 16.02.2024 12:32
+ * © face-recognition - All Rights Reserved
+ */
+public class SampleSize {
+
+  private static int width = 144;
+  private static int height = 144;
+
+  public static void width(int w) {
+    width = w;
+  }
+
+  public static void height(int h) {
+    height = h;
+  }
+
+  public static void size(int s) {
+    width = s;
+    height = s;
+  }
+
+  public static int width() {
+    return width;
+  }
+
+  public static int height() {
+    return height;
+  }
+
+  public static int size() {
+    return width * height;
+  }
+
+}
