8bb9b2e09320b24aae833c69e3155c79804fe324
Clouke <76826179+Clouke@users.noreply.github.com>
2024-02-16 12:50:43 +0100

    minor tweaks

--- src/main/java/gg/clouke/facerec/PipelineBuilder.java
+++ src/main/java/gg/clouke/facerec/PipelineBuilder.java
@@ -1,12 +1,41 @@
 package gg.clouke.facerec;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+
 /**
  * @author Clouke
  * @since 16.02.2024 12:10
  * © face-recognition - All Rights Reserved
  */
-public class PipelineBuilder {
+public class PipelineBuilder<O> {
 
+  private final List<Function<O, O>> stages = new ArrayList<>();
 
+  public PipelineBuilder<O> from(Collection<Function<O, O>> stages) {
+    this.stages.addAll(stages);
+    return this;
+  }
+
+  public PipelineBuilder<O> from(PipelineBuilder<O> builder) {
+    stages.addAll(builder.stages);
+    return this;
+  }
+
+  public PipelineBuilder<O> addStage(Function<O, O> stage) {
+    stages.add(stage);
+    return this;
+  }
+
+  public Pipeline<O> build(Encoder<O> encoder) {
+    return o -> {
+      O out = null;
+      for (Function<O, O> stage : stages)
+        out = stage.apply(o);
+      return encoder.encode(out);
+    };
+  }
 
 }
