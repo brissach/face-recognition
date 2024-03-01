dcd31d4cd87c3665eafa1fddbdb3e68ab6affb5c
Clouke <76826179+Clouke@users.noreply.github.com>
2024-03-01 22:26:35 +0100

    minor changes

--- src/main/java/gg/clouke/facerec/Functions.java
+++ src/main/java/gg/clouke/facerec/Functions.java
@@ -1,6 +1,8 @@
 package gg.clouke.facerec;
 
 import gg.clouke.facerec.filter.EdgeDetection;
+import gg.clouke.facerec.filter.GaussianBlur;
+import gg.clouke.facerec.filter.ImageFilter;
 
 import java.awt.image.BufferedImage;
 import java.util.function.Function;
@@ -12,11 +14,17 @@ import java.util.function.Function;
  */
 public class Functions {
 
-  private static final EdgeDetection EDGE_DET_FILTER = new EdgeDetection();
+  private static final ImageFilter EDGE_DET_FILTER = new EdgeDetection();
+  private static final ImageFilter GAUSSIAN_BLUR_FILTER = new GaussianBlur();
 
   public static final Function<BufferedImage, BufferedImage> RESCALE = image -> {
     int w = image.getWidth();
     int h = image.getHeight();
+
+    if (!SampleSize.initiated())
+      throw new RuntimeException(
+        "Set the sample size using SampleSize#width(size) & SampleSize#height(size) before using the rescale function."
+      );
 
     int sampleW = SampleSize.width();
     int sampleH = SampleSize.height();
@@ -32,5 +40,6 @@ public class Functions {
   };
 
   public static final Function<BufferedImage, BufferedImage> EDGE_DETECTION = EDGE_DET_FILTER::apply;
+  public static final Function<BufferedImage, BufferedImage> GAUSSIAN_BLUR = GAUSSIAN_BLUR_FILTER::apply;
 
 }
