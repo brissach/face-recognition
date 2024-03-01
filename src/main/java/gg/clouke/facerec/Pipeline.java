44ea8d1ffc5d34273e870c7698a117f2b0dd01d7
Clouke <76826179+Clouke@users.noreply.github.com>
2024-03-01 22:26:55 +0100

    add `Encoder<W>#encoder()` & `Pipeline<...>#plain()`

--- src/main/java/gg/clouke/facerec/Pipeline.java
+++ src/main/java/gg/clouke/facerec/Pipeline.java
@@ -4,6 +4,8 @@ import gg.clouke.ContextVector;
 
 import java.awt.image.BufferedImage;
 
+import static gg.clouke.facerec.Functions.*;
+
 /**
  * @author Clouke
  * @since 16.02.2024 12:10
@@ -11,10 +13,22 @@ import java.awt.image.BufferedImage;
  */
 public interface Pipeline<W> {
 
+  static Pipeline<BufferedImage> plain() {
+    return builder()
+      .addStages(
+        EDGE_DETECTION,
+        GAUSSIAN_BLUR,
+        RESCALE
+      )
+      .build(new ImageEncoder());
+  }
+
   static PipelineBuilder<BufferedImage> builder() {
     return new PipelineBuilder<>();
   }
 
   ContextVector process(W w);
+
+  Encoder<W> encoder();
 
 }
