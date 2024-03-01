837d89f51ab953f7a7949e55ddc304ed0f63085c
Clouke <76826179+Clouke@users.noreply.github.com>
2024-03-01 22:27:29 +0100

    add `Map<...>#transferLayers()`

--- src/main/java/gg/clouke/facerec/model/TransferLearningModel.java
+++ src/main/java/gg/clouke/facerec/model/TransferLearningModel.java
@@ -1,9 +1,16 @@
 package gg.clouke.facerec.model;
 
+import gg.clouke.Layer;
+
+import java.util.Map;
+
 /**
  * @author Clouke
  * @since 18.02.2024 11:45
  * © face-recognition - All Rights Reserved
  */
 public interface TransferLearningModel {
+
+  Map<Integer, Layer> transferLayers();
+
 }
